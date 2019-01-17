bc4d78fa661282bf28640f34ad7f3534228c1563
Charles Ris <Charles@macbook-pro-de-charles-2.home>
2019-01-17 14:51:01 +0100

    création de la branche subscription pour solutionner l'ajout d'un abonement dans la bdd

--- controllers/subscription/sub.js
+++ controllers/subscription/sub.js
@@ -1,7 +1,7 @@
 /**
  * Created by Charles on 08/01/2019.
  */
-
+const Subscription = require('../../schema/schemaSubscription.js');
 
 function get(req, res) {
     res.status(200).json({
@@ -9,5 +9,73 @@ function get(req, res) {
     })
 }
 
+function create(req, res) {
+    if (!req.body.name || !req.body.price || req.body.descri || req.body.mensu || req.body.engage) {
+        //Le cas ou il manque des champs
+        res.status(400).json({
+            "text": "Requête invalide"
+        })
+    } else {
+        var subs = {
+            name: req.body.name,
+            company: "Netflix",
+            price: req.body.price,
+            descri: req.body.descri,
+            mensu: req.body.mensu,
+            engage: req.body.engage
+        };
+        var findSubscription = new Promise(function (resolve, reject) {
+            Subscription.find({
+                name: subs.name,
+                company:subs.company
+            }, function (err, result) {
+                if (err) {
+                    reject(500);
+                } else {
+                    if (result) {
+                        reject(204)
+                    } else {
+                        resolve(true)
+                    }
+                }
+            })
+        });
+
+
+        findSubscription.then(function () {
+            var _s = new Subscription(subs);
+            _s.save(function (err, user) {
+                if (err) {
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    })
+                } else {
+                    res.status(200).json({
+                        "text": "Succès"
+                    })
+                }
+            })
+        }, function (error) {
+            switch (error) {
+                case 500:
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    });
+                    break;
+                case 204:
+                    res.status(204).json({
+                        "text": "Le nom de l'abonnement existe déjà"
+                    });
+                    break;
+                default:
+                    res.status(500).json({
+                        "text": "Erreur interne"
+                    })
+            }
+        })
+    }
+}
+
 
 exports.get = get;
+exports.create = create;
--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -2,4 +2,5 @@ const subscription = require('./subscription/sub.js');
 
 module.exports = function (app) {
     app.post('/get', subscription.get);
+    app.post('/create', subscription.create);
 };
